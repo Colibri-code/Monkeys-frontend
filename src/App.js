fb944a6c8474da6188f0d795e77e1bbeaee88591
Wilberth Loría <stwilberth@gmail.com>
2020-08-10 10:04:30 -0600

    instalation base with some examples

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './atoms/main.scss';
+import ActionButtons from './molecules/actionButtons'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <ActionButtons></ActionButtons>
       </header>
     </div>
   );
---
+++ src/molecules/actionButtons.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from './button'
+
+const ActionsButtons = () => {
+    return (
+        <div>
+            <Button name="primary"></Button>
+            <Button name="primary"></Button>
+            <Button name="primary"></Button>
+        </div>
+    );
+}
+    
+export default ActionsButtons;
