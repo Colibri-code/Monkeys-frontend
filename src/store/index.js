6f9c635bcc5e78d036cad99f4a5a32c48952686c
GabrielJosue <gj_alvarado@outlook.com>
2020-10-13 21:12:07 -0600

    menu user Settings

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,14 @@ import { AuthStore } from "./auth/store";
 import { SidebarStore } from "./sidebar/store";
 import { OrganizationStore } from "./organization/store";
 import { HistorialStore } from "./historial/store";
+import {UserSettingsStore} from "./userSettings/store";
 
 const providers = [
   <AuthStore.Provider />,
   <SidebarStore.Provider />,
   <OrganizationStore.Provider />,
   <HistorialStore.Provider />,
+  <UserSettingsStore.Provider/>
 ];
 
 const Store = ({ children: initial }) =>
