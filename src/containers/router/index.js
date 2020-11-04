b1f52c78502b0f7cb18e6997072da819f24dadd0
oscar98colibricode <oscar.alvarez@colibricode.com>
2020-11-04 15:09:43 -0600

    Creacion del componente userManagement, nos permite la administracion de usuarios, de momento al componente le falta la implementacion del filter, cambiar el icono de trash bin por un boton que capture el id y del usuario para poder eliminarlo, falta la funcionalidad del select all y tambien cuando se conecte al business logic cargar los select tags con los distintos roles de usuarios al igual que el cargado independiente del mismo relacionado  con el rol del usuario. Los links de create team o invite teams faltan relacionarlos con sus pantallas independientes

--- src/containers/router/index.js
+++ src/containers/router/index.js
@@ -13,6 +13,7 @@ import Navbar from '../../components/navbar';
 import Sidebar from '../../components/sidebar';
 import TaskDetail from '../../components/task-detail';
 import WikiSection from '../../components/wikiSection';
+import UserManagement from '../../components/userManagement';
 
 // Local
 import useHistorialActions from '../../store/historial/actions';
@@ -43,7 +44,8 @@ function Router() {
                 <Route path='/all-enviroment' component={AllEnviroment} />
                 <Route path='/dev-enviroment' component={DevEnviroment} />
                 <Route path='/task-detail/:id' component={TaskDetail} />
-                <Route path='/wikiSection' component={WikiSection} />
+                <Route path='/wikiSection' component={WikiSection} />                
+                <Route path='/userManagement' component={UserManagement} />
               </Switch>
             </div>
           </div>
