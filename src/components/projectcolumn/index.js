7cc712e2f7dbae44e7d9ea60af5059e4091ac661
Josue943 <josue943@hotmail.com>
2020-11-10 23:16:01 -0600

    drag and drop

--- src/components/projectcolumn/index.js
+++ src/components/projectcolumn/index.js
@@ -1,83 +1,41 @@
-import React, { useState } from "react";
-import { FaCaretDown } from "react-icons/fa";
-import "./style.scss";
-import TaskCard from "../taskcard";
-const ColumnHeader = ({ title, color, number }) => {
+import React, { memo, useState } from 'react';
+import { FaCaretDown } from 'react-icons/fa';
+import { Droppable } from 'react-beautiful-dnd';
+
+import './style.scss';
+
+const ProjectColumn = ({ title, color, number, children }) => {
   const [show, setShow] = useState(false);
   return (
-    <div className="project-column-header">
-      <h5 className="title">
-        {title}
-        <span className="badge-counter" style={{ color }}>
-          {number}
-        </span>
-      </h5>
-      <div className="accordion-icon-container">
-        <FaCaretDown
-          className="accordion-icon"
-          onClick={() => setShow(!show)}
-        />
-      </div>
-    </div>
-  );
-};
-const ProjectColumn = ({ title, color, number, id, tasks = [] }) => {
-  const [show, setShow] = useState(false);
-  const [list, setList] = React.useState(tasks);
-  return (
-    <div
-      id={id}
-      className="project-column"
-      onDragOver={(e) => {
-        e.preventDefault();
-      }}
-      onDrop={(e) => {
-        let id = JSON.parse(e.dataTransfer.getData("id"));
-        console.log("dragged id: ", id);
-        let columnTitle = title;
-        const test = list.filter((task) => {
-          if (task.id == id) {
-            console.log("current state: ", task.state);
-            task.state = columnTitle;
-            console.log("new state: ", task.state);
-          }
-          return task;
-        });
-        setList(test);
-        console.log("test array: ", test);
-      }}
-    >
-      <div className="project-column-header">
-        <h5 className="title">
-          {title}
-          <span className="badge-counter" style={{ color }}>
-            {number}
-          </span>
-        </h5>
-        <div className="accordion-icon-container">
-          <FaCaretDown
-            className="accordion-icon"
-            onClick={() => setShow(!show)}
-          />
-        </div>
-      </div>
-      <div className={`project-tasks-container ${show ? "show-tasks" : ""}`}>
-        {list
-          .filter((task) => task.state == title)
-          .map((filteredTask, i) => (
-            <div
-              onDragStart={(e) => {
-                console.log("Filtered task id: ", filteredTask.id);
-                e.dataTransfer.setData("id", JSON.stringify(filteredTask.id));
-              }}
-              draggable
-              key={i}
-            >
-              <TaskCard task={filteredTask} key={i} color={color} />
+    <Droppable droppableId={title}>
+      {provided => (
+        <div
+          className='project-column'
+          {...provided.droppableProps}
+          key={title}
+          ref={provided.innerRef}
+        >
+          <div className='project-column-header'>
+            <h5 className='title'>
+              {title}
+              <span className='badge-counter' style={{ color }}>
+                {number}
+              </span>
+            </h5>
+            <div className='accordion-icon-container'>
+              <FaCaretDown
+                className='accordion-icon'
+                onClick={() => setShow(!show)}
+              />
             </div>
-          ))}
-      </div>
-    </div>
+          </div>
+          <ul className={`project-tasks-container ${show ? 'show-tasks' : ''}`}>
+            {children}
+          </ul>
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
   );
 };
-export default ProjectColumn;
+export default memo(ProjectColumn);
--- src/pages/manage/index.js
+++ src/pages/manage/index.js
@@ -1,126 +1,178 @@
-import React, { useState } from "react";
-import "./style.scss";
-import ProjectColumn from "../../components/projectcolumn";
-import BreadCrumb from "../../components/breadcrumb";
-import { FaUserAlt } from "react-icons/fa";
-import { FaPlus } from "react-icons/fa";
+import React, { memo, useCallback, useState } from 'react';
+import { FaUserAlt } from 'react-icons/fa';
+import { DragDropContext } from 'react-beautiful-dnd';
+
+import './style.scss';
+import BreadCrumb from '../../components/breadcrumb';
+import ProjectColumn from '../../components/projectcolumn';
+import TaskCard from '../../components/taskcard';
+import tasks from './tasks';
+
 const userCollection = [
-  { id: 1, value: "Oscar Melendez" },
-  { id: 2, value: "Eduardo Alvarez" },
-  { id: 3, value: "Carlos Gutierrez" },
-  { id: 4, value: "Alejandro Melendez" },
-  { id: 5, value: "Cesar Mata" },
-  { id: 6, value: "José Peraza" },
+  { id: 1, value: 'Oscar Melendez' },
+  { id: 2, value: 'Eduardo Alvarez' },
+  { id: 3, value: 'Carlos Gutierrez' },
+  { id: 4, value: 'Alejandro Melendez' },
 ];
+
+const actions = [
+  'Issues',
+  'Planning',
+  'Boards',
+  'Labels',
+  'Service Desk',
+  'Reports',
+];
+
 const Project = () => {
-  const [mode, setMode] = useState("Issues");
-  const actions = [
-    "Issues",
-    "Planning",
-    "Boards",
-    "Labels",
-    "Service Desk",
-    "Reports",
-  ];
-  const tasks = [
-    { id: 1, name: "1", date: "MM/DD/YY", state: "To do" },
-    { id: 2, name: "2", date: "MM/DD/YY", state: "Working" },
-    { id: 3, name: "3", date: "MM/DD/YY", state: "Done" },
-    { id: 4, name: "4", date: "MM/DD/YY", state: "Working" },
-    { id: 5, name: "5", date: "MM/DD/YY", state: "To do" },
-    { id: 6, name: "6", date: "MM/DD/YY", state: "Done" },
-    { id: 7, name: "7", date: "MM/DD/YY", state: "Working" },
-    { id: 8, name: "8", date: "MM/DD/YY", state: "Done" },
-    { id: 9, name: "9", date: "MM/DD/YY", state: "Working" },
-  ];
-  const columns = [
-    { title: "To do", color: "#FF4900", number: 76, id: 1 },
-    { title: "Working", color: "#8798ad", number: 69, id: 2 },
-    { title: "Done", color: "#0070ff", number: 28, id: 3 },
-  ];
+  const [columns, setColumns] = useState({
+    'To do': {
+      color: '#FF4900',
+      number: 76,
+      id: 1,
+      tasks: tasks.filter(i => i.state === 'To do'),
+    },
+    Working: {
+      color: '#8798ad',
+      number: 69,
+      id: 2,
+      tasks: tasks.filter(i => i.state === 'Working'),
+    },
+    Done: {
+      color: '#0070ff',
+      number: 28,
+      id: 3,
+      tasks: tasks.filter(i => i.state === 'Done'),
+    },
+  });
+  const [selectedUser, setSelectedUser] = useState('');
+  const [mode, setMode] = useState('Issues');
+
+  const onDragEnd = ({ destination, source }) => {
+    if (!destination) return;
+    if (
+      destination.index === source.index &&
+      destination.droppableId === source.droppableId
+    ) {
+      return;
+    }
+
+    // Creating a copy of item before removing it from state
+    const itemCopy = { ...columns[source.droppableId].tasks[source.index] };
+    itemCopy.state = destination.droppableId;
+    setColumns(prev => {
+      prev = { ...prev };
+      // Remove from previous items array
+      prev[source.droppableId].tasks.splice(source.index, 1);
+      // Adding to new items array location
+      prev[destination.droppableId].tasks.splice(
+        destination.index,
+        0,
+        itemCopy
+      );
+      return prev;
+    });
+  };
+
+  const setVisible = useCallback(
+    assignee => {
+      if (selectedUser) {
+        if (selectedUser === assignee) return true;
+        else return false;
+      }
+      if (!selectedUser) return true;
+    },
+    [selectedUser]
+  );
+
   return (
-    <div className="manage-page monkeys-p-5">
-      <div className="project-header">
+    <div className='manage-page monkeys-p-5'>
+      <div className='project-header'>
         <BreadCrumb />
-        <div className="monkeys-p-1">
-          <span className="project-type">Public</span>
+        <div className='monkeys-p-1'>
+          <span className='project-type'>Public</span>
         </div>
-        <div className="project-enviroment-buttons">
-          <button className="env-button">All Enviroment</button>
-          <button className="env-button env-active">Dev Enviroment</button>
+        <div className='project-enviroment-buttons'>
+          <button className='env-button'>All Enviroment</button>
+          <button className='env-button env-active'>Dev Enviroment</button>
         </div>
       </div>
-      <div className="project-filter-container">
+      <div className='project-filter-container'>
         {actions.map((action, i) => (
           <button
             key={i}
             onClick={() => setMode(action)}
             className={`project-filter-button ${
-              action === mode ? "filter-active" : ""
+              action === mode ? 'filter-active' : ''
             } `}
           >
             {action}
           </button>
         ))}
       </div>
-      <div className="project-action-container">
-        <div className="project-mode-name">
+      <div className='project-action-container'>
+        <div className='project-mode-name'>
           <h3>Issues</h3>
         </div>
         {/*
           ---------------------------user fields---------------------------------
         */}
-        <UserList />
+        <div className='user-filter'>
+          <ul>
+            {userCollection.map(({ id, value }, i) => (
+              <li key={i}>
+                <UserIcon
+                  id={id}
+                  value={value}
+                  onClick={() => setSelectedUser(id)}
+                  style={selectedUser === id ? 'selected-user' : ''}
+                />
+              </li>
+            ))}
+            <li
+              className='li-clear pointer'
+              onClick={() => setSelectedUser('')}
+            >
+              clear all
+            </li>
+          </ul>
+        </div>
         {/*
             ---------------------------end user fields--------------------------------
         */}
-        <div className="project-action-buttons">
+        <div className='project-action-buttons'>
           <button>Complete Sprint</button>
           <button>Edit Boards</button>
-          <button className="ction-button-special">Share</button>
+          <button className='ction-button-special'>Share</button>
         </div>
       </div>
-      <div className="project-tasks">
-        {columns.map(({ title, number, color, id }, i) => (
-          <ProjectColumn
-            title={title}
-            number={number}
-            color={color}
-            id={id}
-            key={i}
-            tasks={tasks}
-          />
-        ))}
+      <div className='project-tasks'>
+        <DragDropContext onDragEnd={onDragEnd}>
+          {Object.keys(columns).map((i, index) => (
+            <ProjectColumn title={i} {...columns[i]} key={index}>
+              {columns[i].tasks.map((task, i) => (
+                <TaskCard
+                  {...task}
+                  key={i}
+                  index={i}
+                  visible={setVisible(task.assignee)}
+                />
+              ))}
+            </ProjectColumn>
+          ))}
+        </DragDropContext>
       </div>
     </div>
   );
 };
-const UserList = () => {
+
+const UserIcon = ({ id, value, onClick, style }) => {
   return (
-    <div className="user-filter">
-      <ul>
-        {userCollection.map(({ id, value }, i) => (
-          <li key={i}>
-            <UserIcon id={id} value={value} />
-          </li>
-        ))}
-        {/*
-        <li><a><span><FaPlus/></span></a></li>
-        */}
-      </ul>
+    <div className={'icon-container ' + style} onClick={onClick}>
+      <input id={id} type='checkbox' value={value} />
+      <FaUserAlt className='user-icon-center' />
     </div>
   );
 };
-const UserIcon = (props) => {
-  return (
-    <div className="icon-container">
-      <label className="userIcon">
-        <input id={props.id} type="checkbox" value={props.value} />
-        <div className="toggle">
-          <FaUserAlt />
-        </div>
-      </label>
-    </div>
-  );
-};
-export default Project;
+
+export default memo(Project);
