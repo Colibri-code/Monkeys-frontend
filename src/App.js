5d7e5dcd8accb129f41d77934a279807086f2c4f
oscar98colibricode <oscar.alvarez@colibricode.com>
2020-12-03 18:45:14 -0600

    Este cambio en el file app.js realiza una conexion de prueba con el lado del backend, como tal esta parte no presenta problemas con los cors del lado backend.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 // import $ from 'jquery';
 // import Popper from 'popper.js';
 import Router from './containers/router';
+import Axios from 'axios';
+
 
 function App() {
+  useEffect(()=> {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const data = await Axios('http://localhost:1337/user/read/1');
+    console.log('Esta es la prueba de backend con frontend', data);
+  };
+
   return <Router />;
 }
 
